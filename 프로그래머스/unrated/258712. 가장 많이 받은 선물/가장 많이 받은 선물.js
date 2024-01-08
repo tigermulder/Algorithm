6549e2672eacc3e45e1e18f85d8be9ac4212984d
weekendtiger <97021279+tigermulder@users.noreply.github.com>
2024-01-08 12:36:45 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 4.07 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,63 +1,38 @@
 function solution(friends, gifts) {
   let answer = {}
-  let fromTo = {}
-  let people = {}
-  let check = {}
-  for (const i of friends) {
-    answer[i] = 0
-    people[i] = [0,0,0]
-  }
-  friends.forEach(name => {
-    fromTo[name] = {}
-    check[name] = {}
-    friends.forEach(inner => {
-      if(name !== inner){
-        fromTo[name][inner] = 0
-        check[name][inner] = false
+  let statistic = {}
+  let idxStatistic = {}
+  for (const name of friends) {
+    answer[name] = 0
+    statistic[name] = 0
+    idxStatistic[name] = {}
+    for (const sub of friends) {
+      if(name !== sub){
+        idxStatistic[name][sub] = 0
       }
-    })
-  });
+    }
+  }
   for (const cur of gifts) {
     let [from,to] = cur.split(' ')
-    people[from][0] = people[from][0]+1
-    people[from][2] = people[from][2]+1
-    people[to][1] = people[to][1]+1
-    people[to][2] = people[to][2]-1
-    fromTo[from][to] = fromTo[from][to]+1
+    statistic[from] = statistic[from]+1
+    statistic[to] = statistic[to]-1
+    idxStatistic[from][to] = idxStatistic[from][to]+1
   }
-  for (const cur in people) {
-    for (const i in fromTo[cur]) {
-      let gift = fromTo[cur][i] // 내가 특정인물에게 준 선물준갯수
-      let received = fromTo[i][cur] // 내가 선물을한 준특정인물이 나에게준 선물갯수
-      if(gift > 0 || received > 0){
-        if(gift === received){
-          if(check[i][cur] === false && check[cur][i] === false){
-            check[i][cur] = true
-            check[cur][i] = true
-            if(people[i][2] < people[cur][2]){
-              answer[cur] = answer[cur]+1
-            }else if(people[cur][2] < people[i][2]){
-              answer[i] = answer[i]+1
-            }
-          }
-        }else{
-          if(gift > received){
-            answer[cur] = answer[cur]+1
-          }
+  for (const from in idxStatistic) {
+    for (const to in idxStatistic[from]) {
+      if(idxStatistic[from][to] === 0 && idxStatistic[to][from] === 0 || idxStatistic[from][to] === idxStatistic[to][from]){
+        if(statistic[from] > statistic[to]){
+          answer[from] = answer[from]+1
         }
-      }else if(gift === 0 && received === 0){
-        if(check[i][cur] === false && check[cur][i] === false){
-          check[i][cur] = true
-          check[cur][i] = true
-          if(people[i][2] < people[cur][2]){
-            answer[cur] = answer[cur]+1
-          }else if(people[cur][2] < people[i][2]){
-            answer[i] = answer[i]+1
-          }
+      }else{
+        if(idxStatistic[from][to] < idxStatistic[to][from]){
+          answer[to] = answer[to]+1
+        }else if(idxStatistic[to][from] > idxStatistic[from][to]){
+          answer[from] = answer[from]+1
         }
       }
     }
   }
-  answer = Object.values(answer)
-  return Math.max(...answer)
+  answer = Math.max(...Object.values(answer))
+  return answer
 }
